86c34335aa3954c6220d1d916d3ae7f1812f5d32
Amir <amir.husain@getfareye.com>
2022-02-23 11:16:49 +0530

    gadient applied

--- lib/flutter_pkg.dart
+++ lib/flutter_pkg.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, deprecated_member_use
+
 library flutter_pkg;
 
 import 'package:flutter/material.dart';
@@ -12,17 +14,36 @@ class CustomButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextButton(
-        onPressed: onPressed,
-        style: TextButton.styleFrom(
-          padding: const EdgeInsets.all(16.0),
-          primary: Colors.white,
-          backgroundColor: Colors.purple,
-          elevation: 9.0,
-          textStyle: const TextStyle(
-            fontSize: 20,
-          ),
+    return Container(
+      margin: EdgeInsets.all(20),
+      width: 200,
+      height: 50,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.red, Colors.blue],
+          begin: FractionalOffset.centerLeft,
+          end: FractionalOffset.centerRight,
         ),
-        child: child);
+      ),
+      child: FlatButton(
+        child: Text(
+          "Click Me",
+          style: TextStyle(color: Colors.white),
+        ),
+        onPressed: onPressed,
+      ),
+    );
+    // return TextButton(
+    //     onPressed: onPressed,
+    //     style: TextButton.styleFrom(
+    //       padding: const EdgeInsets.all(16.0),
+    //       primary: Colors.white,
+    //       backgroundColor: Colors.purple,
+    //       elevation: 9.0,
+    //       textStyle: const TextStyle(
+    //         fontSize: 20,
+    //       ),
+    //     ),
+    //     child: child);
   }
 }
